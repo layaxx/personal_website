f2b73602683224c4816ba4b7e9f7a701d9b5e3e5
Yannick Lang <layaxx@gmx.de>
2021-06-01 11:15:46 +0200

    update content

--- scripts/sb-watch.js
+++ scripts/sb-watch.js
@@ -64,12 +64,12 @@ function _processFile(filePath, watchEvent) {
 
 function _handlePug(filePath, watchEvent) {
     if (watchEvent === 'change') {
-        if (filePath.match(/includes/) || filePath.match(/mixins/) || filePath.match(/\/pug\/layouts\//)) {
+        if (filePath.match(/include/) || filePath.match(/includes/) || filePath.match(/mixins/) || filePath.match(/\/pug\/layouts\//)) {
             return _renderAllPug();
         }
         return renderPug(filePath);
     }
-    if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
+    if (!filePath.match(/include/) && !filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
         return renderPug(filePath);
     }
 }
