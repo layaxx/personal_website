2b32020f19e38f676a168c86720d2e914cb085a6
Yannick Lang <layaxx@gmx.de>
2026-01-07 11:15:14 +0100

    update dependencies to node v22

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
+# personal website
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/a473c55c-1b81-4004-bdbf-d53ec57e4953/deploy-status)](https://app.netlify.com/sites/yannicklang/deploys)
 
-# personal website
 This repository contains the source files for my personal Website, which showcases my projects.
 
 *currently a work in progress*
-
-
--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -1,32 +1,37 @@
-'use strict';
-const autoprefixer = require('autoprefixer');
-const fs = require('fs');
-const packageJSON = require('../package.json');
-const path = require('path');
-const postcss = require('postcss');
-const sass = require('sass');
-const sh = require('shelljs');
+"use strict"
+const autoprefixer = require("autoprefixer")
+const fs = require("fs")
+const packageJSON = require("../package.json")
+const path = require("path")
+const postcss = require("postcss")
+const sass = require("sass")
+const sh = require("shelljs")
 
-const stylesPath = '../src/scss/styles.scss';
-const destPath = path.resolve(path.dirname(__filename), '../dist/css/styles.css');
+const stylesPath = "../src/scss/styles.scss"
+const destPath = path.resolve(
+  path.dirname(__filename),
+  "../dist/css/styles.css"
+)
 
 module.exports = function renderSCSS() {
-    const results = sass.renderSync({
-        data: entryPoint,
-        includePaths: [path.resolve(path.dirname(__filename), '../node_modules')]
-    });
+  const results = sass.renderSync({
+    data: entryPoint,
+    includePaths: [path.resolve(path.dirname(__filename), "../node_modules")],
+  })
 
-    const destPathDirname = path.dirname(destPath);
-    if (!sh.test('-e', destPathDirname)) {
-        sh.mkdir('-p', destPathDirname);
-    }
+  const destPathDirname = path.dirname(destPath)
+  if (!sh.test("-e", destPathDirname)) {
+    sh.mkdir("-p", destPathDirname)
+  }
 
-    postcss([autoprefixer]).process(results.css, { from: 'styles.css', to: 'styles.css' }).then((result) => {
-        result.warnings().forEach((warn) => {
-            console.warn(warn.toString());
-        });
-        fs.writeFileSync(destPath, result.css.toString());
-    });
-};
+  postcss([autoprefixer])
+    .process(results.css, { from: "styles.css", to: "styles.css" })
+    .then((result) => {
+      result.warnings().forEach((warn) => {
+        console.warn(warn.toString())
+      })
+      fs.writeFileSync(destPath, result.css.toString())
+    })
+}
 
-const entryPoint = `@import "${stylesPath}"`;
+const entryPoint = `@use "${stylesPath}"`
